94b2239abfde09d0619a63e250c2bd6383046ffd
Digital-UNI Build <dev@digital-uni.net>
2026-08-22 20:47:35 -0700

    Fix next-intl request locale configuration

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,10 +2,13 @@ import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { isValidLocale } from './config';
 
-export default getRequestConfig(async ({ locale }) => {
-  if (!isValidLocale(locale)) notFound();
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale;
+
+  if (!locale || !isValidLocale(locale)) notFound();
 
   return {
+    locale,
     messages: (await import(`../../messages/${locale}.json`)).default
   };
 });
